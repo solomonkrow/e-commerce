3423890ce657d8a992a788772dfebc0cd5867f1e
solomonkrow <metro317@live.com>
2023-10-27 22:29:40 -0500

    established relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  foreignKey: '',
+  onDelete: '',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete: '',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+      model: ProductTag
+  },
+  as: 'product_id'
+});
 
 module.exports = {
   Product,
