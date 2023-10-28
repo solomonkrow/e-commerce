23842f0d05ec817b79b376241c6157b6431ef5a3
solomonkrow <metro317@live.com>
2023-10-28 00:02:42 -0500

    started routes

--- models/index.js
+++ models/index.js
@@ -5,15 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsToMany(Category, {
-  foreignKey: '',
-  onDelete: '',
+Product.belongsTo(Category, {
+  foreignKey: 'product_tag',
+  onDelete: 'SET NULL',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: '',
-  onDelete: '',
+  foreignKey: 'product_tag',
+  onDelete: 'SET NULL',
 });
 
 // Products belongToMany Tags (through ProductTag)
